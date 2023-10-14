e437a1f0801734cb9a162f26d8017ab790533828
Sondre Engen <corastweb94@hotmail.com>
2023-10-14 15:40:36 +0200

    Update
    Signed-off-by: Sondre Engen <corastweb94@hotmail.com>

--- flake.nix
+++ flake.nix
@@ -3,23 +3,28 @@
 
   inputs = {
     # Software source
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     # Manages configs and software install by symlinking
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  outputs = inputs: {
-    defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
-    defaultPackage.x86_64-darwin = home-manager.defaultPackage.x86_64-darwin;
- 
-    homeConfigurations = {
-      "engson1" = inputs.home-manager.lib.homeManagerConfiguration {
-        system = "x86_64-linux ";
-        homeDirectory = "/home/engson1";
-        username = "engson1"; 
-        configuration.imports = [ ./home.nix ];
-      };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations."engson" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
 }
