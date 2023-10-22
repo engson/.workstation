c6cc0bd1952e286e04aedaa1718233819fc3c2fb
Sondre Engen <corastweb94@hotmail.com>
2023-10-22 13:28:08 +0200

    Update
    Signed-off-by: Sondre Engen <corastweb94@hotmail.com>

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     # Software source
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Flake util functions
+    flake-utils.url = "github:numtide/flake-utils";
     # Manages configs and software install by symlinking
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -11,21 +13,22 @@
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      defaultPackage.${system} = home-manager.defaultPackage.${system};
-      homeConfigurations."engson" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+  outputs = { nixpkgs, flake-utils, home-manager, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        user = "$USERNAME";
+      in {
+        defaultPackage.${system} = home-manager.defaultPackage.${system};
+        homeConfigurations.engson = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home.nix ];
+          # Specify your home configuration modules here, for example,
+          # the path to your home.nix.
+          modules = [ ./home.nix ];
 
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
-      };
-    };
+          # Optionally use extraSpecialArgs
+          # to pass through arguments to home.nix
+        };
+      });
 }
