7838fa32e49d63cbd5cd2a4ac6acbf382588b594
Sondre Engen <corastweb94@hotmail.com>
2024-03-10 10:53:56 +0100

    Uncomment bug

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -9,7 +9,7 @@
 
     # You can also split up your configuration and import pieces of it here:
     # ./nvim.nix
-    ../home/engson/features/ripgrep
+    # ../home/engson/features/ripgrep
   ];
 
   nixpkgs = {
