000fec51488309a01dd3e04d59a592abf78598a8
engson <corastweb94@hotmail.com>
2025-12-20 16:19:06 +0100

    Update 2

--- home/engson/laptop.nix
+++ home/engson/laptop.nix
@@ -1,4 +1,4 @@
-{ inputs, outputs, lib, config, pkgs, ... }: {
+{ pkgs, inputs, outputs, lib, config, pkgs, ... }: {
   # You can import other home-manager modules here
   imports = [
     # If you want to use modules your own flake exports (from modules/home-manager):
@@ -38,19 +38,6 @@
       allowUnfreePredicate = _: true;
     };
   };
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  # home.username = "${username}";
-  # home.homeDirectory = "/home/${username}";
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  # home.stateVersion = "23.05"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
