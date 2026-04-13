8f9f42dd037324b5517d83498ed2792474fcf532
engson <corastweb94@hotmail.com>
2026-04-13 20:37:01 +0200

    Add factor

--- flake.nix
+++ flake.nix
@@ -5,18 +5,20 @@
   # Declares flake inputs
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/25.11";
-
+    # Allow creating modular, composable and reusable flakes
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
-
+    # Allow recursivly importing .nix file (modules)
     import-tree.url = "github:vic/import-tree";
-
+    # Allow wrapping packages into self-contained derivatives.
     wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
 
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    hjem = {
+      url = "github:feel-co/hjem";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
