883668e22dc3ec21e7119b5ccce5eb64f6f4d8ea
engson <corastweb94@hotmail.com>
2026-05-31 17:24:59 +0200

    Upgrade os

--- basic/flake.nix
+++ basic/flake.nix
@@ -2,7 +2,7 @@
   description = "Minimal NixOS flake from existing config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
